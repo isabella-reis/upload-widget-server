1f764f48376dcca6ea1d25bfbe7681da22cad559
Isabella Reis <isabellanery15@hotmail.com>
2025-03-15 23:09:43 -0300

    Swagger config

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,7 +1,26 @@
 import { FastifyPluginAsync } from "fastify";
+import { z } from "zod";
 
-export const uploadImageRoute: FastifyPluginAsync = async server => {
-    server.post('/uploads', () => {
-        return 'Olá Mundo'
-    })
-}
+export const uploadImageRoute: FastifyPluginAsync = async (server) => {
+  server.post(
+    '/uploads',
+    {
+      schema: {
+        summary: "Upload an image",
+        body: z.object({
+          name: z.string(),
+          password: z.string().optional(),
+        }),
+        response: {
+          201: z.object({ uploadId: z.string() }),
+          409: z
+            .object({ message: z.string() })
+            .describe("Upload already exists in server."),
+        },
+      },
+    },
+    async (request, reply) => {
+      return reply.status(201).send({ uploadId: "test" });
+    }
+  );
+};
--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,17 +1,25 @@
 import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
 import { env } from "../../env";
-import { serializerCompiler, validatorCompiler, hasZodFastifySchemaValidationErrors } from "fastify-type-provider-zod";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
+} from "fastify-type-provider-zod";
 import { uploadImageRoute } from "./routes/upload-image";
+import { fastifyMultipart } from "@fastify/multipart";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
 
 const server = fastify();
 
-server.setValidatorCompiler(validatorCompiler)
-server.setSerializerCompiler(serializerCompiler)
+server.setValidatorCompiler(validatorCompiler);
+server.setSerializerCompiler(serializerCompiler);
 
 /**
  * Manipulador global de erros do servidor.
- * 
+ *
  * @param {Error} error - O erro capturado durante a execução.
  * @param {import('fastify').FastifyRequest} request - O objeto da requisição.
  * @param {import('fastify').FastifyReply} reply - O objeto de resposta do Fastify.
@@ -19,24 +27,37 @@ server.setSerializerCompiler(serializerCompiler)
  */
 
 server.setErrorHandler((error, request, reply) => {
-    if (hasZodFastifySchemaValidationErrors(error)) {
-        return reply.status(400).send({
-            message: 'Validation error.',
-            issues: error.validation,
-        })
-    }
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: "Validation error.",
+      issues: error.validation,
+    });
+  }
 
-    console.log(error) // Substituiríamos por um logger que enviaria o erro para o Datadog (ex.:)
+  console.log(error); // Substituiríamos por um logger que enviaria o erro para o Datadog (ex.:)
 
-    return reply.status(500).send({message: 'Internal server error.'})
-})
+  return reply.status(500).send({ message: "Internal server error." });
+});
 
-server.register(uploadImageRoute)
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Upload Server",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+server.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
 
-server.register(fastifyCors, {origin: '*'});
+server.register(fastifyMultipart);
 
-console.log(env.DATABASE_URL)
+server.register(uploadImageRoute);
 
-server.listen({port: 3333, host: '0.0.0.0'}).then(() => {
-    console.log('HTTP server running')
-})
+server.register(fastifyCors, { origin: "*" });
+
+server.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
+  console.log("HTTP server running");
+});
